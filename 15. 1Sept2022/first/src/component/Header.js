4e43fbad05a8bded575b23d07ccb3ab616fc1032
vanshajbajaj <vanshaj1762.be20@chitkara.edu.in>
2022-09-06 10:59:44 +0530

    6 sept 2022 React Styling

--- 15. 1Sept2022/first/src/component/Header.js
+++ 15. 1Sept2022/first/src/component/Header.js
@@ -1,8 +1,23 @@
+import './Header.css';
+
 const Header = (props) => {
     return (
-        <header><h1 style={{color:props.color}}>Hello {props.title}, color is {props.color}</h1></header>
+        <header>
+            <h1 style={headingStyle}>Hello {props.title}, color is {headingStyle.color}</h1>
+            <h2 className="h2Head">H2 heading</h2>
+            <h3 className="h3Head">H3 heading</h3>
+            <h4 className="abc">H4 heading</h4>
+        </header>
     );
 };
+
+const headingStyle={
+    color:'darkcyan',
+    backgroundColor:'lightblue',
+    textAlign:'center',
+
+    
+}
 
 Header.defaultProps={
     color:"green",
